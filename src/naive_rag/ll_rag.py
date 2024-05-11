6352b72e71241fdce0e2d4c9a273325ed89b4907
unclefomotw <unclefomotw@gmail.com>
2024-05-11 19:53:00 +0800

    (LlamaIndex) Generate prompt using content only
    See implementation of SimpleDirectoryReader for details:
    During the ETL/index, several metadata such as file_name, file_path,
    are stored in the index.  Despite some metadata by default are excluded
    for llm generation, somehow SimpleDirectoryReader thinks file_path is
    important and not excluded.
    
    For the purpose of the demo, I just want to include the text content
    as the prompt to LLM

--- src/naive_rag/ll_rag.py
+++ src/naive_rag/ll_rag.py
@@ -1,10 +1,14 @@
 import os
+from typing import List, Optional
 
 from llama_index.core import VectorStoreIndex, get_response_synthesizer
+from llama_index.core.bridge.pydantic import Field
 from llama_index.core.indices.vector_store import VectorIndexRetriever
 from llama_index.core.llms import ChatMessage, MessageRole
+from llama_index.core.postprocessor.types import BaseNodePostprocessor
 from llama_index.core.prompts.base import ChatPromptTemplate
 from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core.schema import NodeWithScore, QueryBundle
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.openai import OpenAI
 from llama_index.vector_stores.qdrant import QdrantVectorStore
@@ -14,23 +18,50 @@ from src.naive_rag.constants import SYSTEM_PROMPT
 from src.naive_rag.constants import USER_PROMPT
 
 
-def get_customized_prompt_template():
+class MetadataExclusionPostProcessor(BaseNodePostprocessor):
+    """Exclude specified metadata keys from LLM synthesis."""
+
+    excluding_metadata_keys: list = Field(
+        description="Metadata keys to exclude from generating the prompt."
+    )
+
+    def __init__(self, excluding_metadata_keys: list) -> None:
+        super().__init__(excluding_metadata_keys=excluding_metadata_keys)
+
+    @classmethod
+    def class_name(cls) -> str:
+        return "MetadataExclusionPostProcessor"
+
+    def _postprocess_nodes(
+            self,
+            nodes: List[NodeWithScore],
+            query_bundle: Optional[QueryBundle] = None,
+    ) -> List[NodeWithScore]:
+        for n in nodes:
+            n.node.excluded_llm_metadata_keys.extend(self.excluding_metadata_keys)
+
+        return nodes
+
+
+def _get_customized_prompt_template():
     """Prompt that passes the question and retrieved context to LLM."""
     system_prompt = ChatMessage(role=MessageRole.SYSTEM, content=SYSTEM_PROMPT)
     user_prompt = ChatMessage(role=MessageRole.USER, content=USER_PROMPT)
 
+    # "query_str" is a magic word defined in llamaindex `CompactAndRefine`
+    # "context_str" is a magic word defined in llamaindex `Refine`
     return ChatPromptTemplate(
         message_templates=[system_prompt, user_prompt],
         template_var_mappings={"context_str": "context", "query_str": "query"}
     )
 
 
-def get_customized_llm():
+def _get_customized_llm():
     """LLM that synthesizes the answer from the prompt"""
-    return OpenAI(temperature=0.0001)
+    return OpenAI(model="gpt-3.5-turbo", temperature=0.0001)
 
 
-def get_retriever():
+def _get_retriever():
     db_client = QdrantClient(host="localhost", port=6333)
     vector_store = QdrantVectorStore(
         collection_name="naive_rag_ll",
@@ -56,17 +87,22 @@ def get_retriever():
 
 
 def query():
-    retriever = get_retriever()
-    prompt_template = get_customized_prompt_template()
-    llm = get_customized_llm()
+    retriever = _get_retriever()
+    prompt_template = _get_customized_prompt_template()
+    llm = _get_customized_llm()
 
     response_synthesizer = get_response_synthesizer(
         llm=llm,
         text_qa_template=prompt_template,
     )
 
+    # can also remove those metadata keys back in the ETL stage
+    # especially when we don't use them to filter/search
     query_engine = RetrieverQueryEngine(
         retriever=retriever,
+        node_postprocessors=[
+            MetadataExclusionPostProcessor(['page_label', 'file_path'])
+        ],
         response_synthesizer=response_synthesizer
     )
 
